bce1fcfa1fbbfde3af6388aef6f1831014bcdb33
Lexa Huang <sixian.duidui@gmail.com>
2024-04-25 19:33:24 -0400

    Implement quiz results page

--- server.py
+++ server.py
@@ -8,110 +8,141 @@ app = Flask(__name__)
 answers = {}
 
 
-
 postures = {
-    "1":{
+    "1": {
         "item_id": "1",
         "title": "Forward Head Posture",
         "image": "https://static1.squarespace.com/static/5b4e58369d5abb9d6adac232/5b50dfab575d1ff383c06981/5c73effee79c704c50cf605d/1619981137718/Figure_18.4.png?format=300w",
-        "text": [["Headaches","Fatigue","Balance issues"],["Pain and stiffness","Impaired movement"],["Pain and stiffness","Impaired movement","Respiratory issues"]]
+        "text": [
+            ["Headaches", "Fatigue", "Balance issues"],
+            ["Pain and stiffness", "Impaired movement"],
+            ["Pain and stiffness", "Impaired movement", "Respiratory issues"],
+        ],
     },
-    "2":{
+    "2": {
         "item_id": "2",
         "title": "Lower Crossed",
         "image": "https://www.yoganatomy.com/wp-content/uploads/2020/10/lcs-diagram.jpg",
-        "text": [["Protruding stomach"],["Lower back pain"],["Pain and stiffness"]]
-    }
+        "text": [["Protruding stomach"], ["Lower back pain"], ["Pain and stiffness"]],
+    },
 }
 
 anatomies = {
-    "1":{
+    "1": {
         "item_id": "1",
         "title": "Muscles",
-        "text": "Support and stabilize the skeletal system, aiding in proper alignment and resisting postural imbalances."
+        "text": "Support and stabilize the skeletal system, aiding in proper alignment and resisting postural imbalances.",
     },
-    "2":{
+    "2": {
         "item_id": "2",
         "title": "Joints",
-        "text": "Allow movement and contribute to posture; proper alignment of joints ensures balance and stability."
+        "text": "Allow movement and contribute to posture; proper alignment of joints ensures balance and stability.",
     },
-    "3":{
+    "3": {
         "item_id": "3",
         "title": "Spine",
-        "text": "Comprised of vertebrae and discs, maintaining its natural curves—cervical, thoracic, lumbar, and sacral—is key for good posture."
+        "text": "Comprised of vertebrae and discs, maintaining its natural curves—cervical, thoracic, lumbar, and sacral—is key for good posture.",
     },
-    "4":{
+    "4": {
         "item_id": "3",
         "title": "Connective Tissues",
-        "text": "Ligaments and tendons connect bones and muscles, providing support and stability crucial for posture."
-    }
+        "text": "Ligaments and tendons connect bones and muscles, providing support and stability crucial for posture.",
+    },
 }
 
 stretcheslist = {
-    "1":{
+    "1": {
         "item_id": "1",
         "action": "strengthen",
         "target": "weak",
-        "title":"Standing Chin Tucks",
+        "title": "Standing Chin Tucks",
         "media": "https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/07/Chin-tucks.gif?w=400",
-        "text": ["Strengthen deep neck flexors","Stand with the upper back against the wall, with the feet shoulder-width apart.", "Tuck the chin in and hold for a few seconds.", "Return to the starting position and repeat a number of times."]
+        "text": [
+            "Strengthen deep neck flexors",
+            "Stand with the upper back against the wall, with the feet shoulder-width apart.",
+            "Tuck the chin in and hold for a few seconds.",
+            "Return to the starting position and repeat a number of times.",
+        ],
     },
-    "2":{
+    "2": {
         "item_id": "2",
         "action": "strengthen",
         "target": "weak",
-        "title":"Floor Cobras",
+        "title": "Floor Cobras",
         "media": "https://www.ocregister.com/wp-content/uploads/migration/kpi/kpinsg-07exercicelg.jpg?w=400",
-        "text": ["Strengthen lower trapz and serratus anterior ","Lie on the floor, arms at side of body (or with arms in front of body in a “Superman” position), palms facing toward ground.", "Pinch shoulder blades together and lift chest off the floor. Hold for 2 seconds. Slowly return body to the ground, keeping chin tucked."]
+        "text": [
+            "Strengthen lower trapz and serratus anterior ",
+            "Lie on the floor, arms at side of body (or with arms in front of body in a “Superman” position), palms facing toward ground.",
+            "Pinch shoulder blades together and lift chest off the floor. Hold for 2 seconds. Slowly return body to the ground, keeping chin tucked.",
+        ],
     },
-    "3":{
+    "3": {
         "item_id": "3",
         "action": "strengthen",
         "target": "weak",
-        "title":"Wall Angels",
+        "title": "Wall Angels",
         "media": "https://media.post.rvohealth.io/wp-content/uploads/sites/2/2021/04/GRT-1.7.Wall-Angels.gif",
-        "text": ["Strengthen back muscles","Stand against the wall: Align back and head; engage core.", "Raise arms in “V,” adjust feet for alignment.", "Bend elbows, lower hands above shoulders; hold, then return. Repeat 5-10x."]
+        "text": [
+            "Strengthen back muscles",
+            "Stand against the wall: Align back and head; engage core.",
+            "Raise arms in “V,” adjust feet for alignment.",
+            "Bend elbows, lower hands above shoulders; hold, then return. Repeat 5-10x.",
+        ],
     },
-    "4":{
+    "4": {
         "item_id": "4",
         "action": "stretch",
         "target": "tight",
-        "title":"Upper Trapz Stretch",
+        "title": "Upper Trapz Stretch",
         "media": "https://www.athletico.com/wp-content/uploads/2017/12/Stretch-of-the-Week-Self-Trapezius-Traction-Stretch-608x608.jpg",
-        "text": ["Stretch upper trapezius and levator scapulae", "Tuck chin and slowly draw left ear to left shoulder.", "Continue by rotating chin downward until a slight stretch is felt on the right side.", "Perform the sequence on both sides, holding each stretch position for 20-30 seconds."]
+        "text": [
+            "Stretch upper trapezius and levator scapulae",
+            "Tuck chin and slowly draw left ear to left shoulder.",
+            "Continue by rotating chin downward until a slight stretch is felt on the right side.",
+            "Perform the sequence on both sides, holding each stretch position for 20-30 seconds.",
+        ],
     },
-    "5":{
+    "5": {
         "item_id": "5",
         "action": "stretch",
         "target": "tight",
-        "title":"Chest Stretch",
+        "title": "Chest Stretch",
         "media": "https://images.squarespace-cdn.com/content/v1/5a620a85d55b41e7233c5243/ae25b65f-ef76-40f5-8091-76cb07bdad9b/PleasantTerrificBettong-size_restricted.gif",
-        "text": ["Stretch pecs", "Stand upright with the feet shoulder-width apart.", "Bring the shoulders back and down.", "Interlace the fingers behind the back with the palms up.", "Draw the shoulders back and down, ensuring that the elbows are straight and that there is no arch in the low back."]
-    }
+        "text": [
+            "Stretch pecs",
+            "Stand upright with the feet shoulder-width apart.",
+            "Bring the shoulders back and down.",
+            "Interlace the fingers behind the back with the palms up.",
+            "Draw the shoulders back and down, ensuring that the elbows are straight and that there is no arch in the low back.",
+        ],
+    },
 }
 
 back_stretchlist = {
     "1": {
         "item_id": "1",
         "part": "Upper Back",
-        "image" : "../../../static/upper.png",
-        "stretch_names": ["Mindful shoulder circles", "Thoracic extension stretch", "Angel wings", "Shoulder stretch", "Neck stretch"]
+        "image": "../../../static/upper.png",
+        "stretch_names": [
+            "Mindful shoulder circles",
+            "Thoracic extension stretch",
+            "Angel wings",
+            "Shoulder stretch",
+            "Neck stretch",
+        ],
     },
-    "2":
-    {
+    "2": {
         "item_id": "2",
         "part": "Lower Back",
-        "image" : "../../../static/lower.png",
-        "stretch_names": ["Cat cow", "Extended puppy pose", "Locust Pose"]
+        "image": "../../../static/lower.png",
+        "stretch_names": ["Cat cow", "Extended puppy pose", "Locust Pose"],
     },
-    "3":
-    {
+    "3": {
         "item_id": "3",
         "part": "Both",
-        "image" : "../../../static/both.png",
-        "stretch_names": ["Thoracic rotation", "Sphinx", "Dynamic camel"]
+        "image": "../../../static/both.png",
+        "stretch_names": ["Thoracic rotation", "Sphinx", "Dynamic camel"],
     },
-
 }
 
 
@@ -120,61 +151,73 @@ back_stretchlist = {
 def homepage():
     return render_template("homepage.html")
 
-@app.route('/favicon.ico')
+
+@app.route("/favicon.ico")
 def favicon():
-    return '', 204
+    return "", 204
 
 
 @app.route("/posture/<int:item_id>")
 def posture(item_id):
     pos = postures[str(item_id)]
     if item_id == 2:
-        return render_template('learn/posture/lower_crossed.html', id=item_id, pos=pos)
+        return render_template("learn/posture/lower_crossed.html", id=item_id, pos=pos)
     else:
-        return render_template('learn/posture/forward_head.html', id=item_id, pos=pos)
+        return render_template("learn/posture/forward_head.html", id=item_id, pos=pos)
 
 
 @app.route("/anatomy")
 def anatomy():
-    #anatomy = anatomies[str(item_id)]
-    #print(anatomies)
-    prev_url = '/posture/1'
-    next_url = '/stretches/1'
-    return render_template('learn/anatomy/anatomy.html', anatomies=anatomies, next_url=next_url, prev_url=prev_url)
+    # anatomy = anatomies[str(item_id)]
+    # print(anatomies)
+    prev_url = "/posture/1"
+    next_url = "/stretches/1"
+    return render_template(
+        "learn/anatomy/anatomy.html",
+        anatomies=anatomies,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
 
 @app.route("/stretches/<int:item_id>")
 def stretches(item_id):
     stretch = stretcheslist[str(item_id)]
     if item_id == 1:
-        prev_url = '/anatomy'
+        prev_url = "/anatomy"
     else:
-        prev_url = str(item_id-1)
+        prev_url = str(item_id - 1)
     if item_id == len(stretcheslist):
-        next_url = '/stretches/back_stretches'
+        next_url = "/stretches/back_stretches"
     else:
-        next_url = str(item_id+1)
-    return render_template('learn/stretches/stretch.html', id=item_id, stretch=stretch, next_url=next_url, prev_url=prev_url)
+        next_url = str(item_id + 1)
+    return render_template(
+        "learn/stretches/stretch.html",
+        id=item_id,
+        stretch=stretch,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
-@app.route('/stretches/back_stretches')
+
+@app.route("/stretches/back_stretches")
 def back_stretches():
-    return render_template('learn/stretches/back_stretches.html', back_stretchlist=back_stretchlist)
+    return render_template(
+        "learn/stretches/back_stretches.html", back_stretchlist=back_stretchlist
+    )
+
 
 @app.route("/quiz/<int:item_id>")
 def quiz(item_id):
 
     global answers
 
-    # reset user's answers if we are restarting from first question
-    # if item_id == 1:
-    #     answers = []
-
-    #check whether user has already answered this question
+    # check whether user has already answered this question
     if item_id in answers:
-        is_answered = True 
+        is_answered = True
         user_answer = answers[item_id]
     else:
-        is_answered = False 
+        is_answered = False
         user_answer = None
 
     quiz_data = questions.get(item_id)
@@ -191,32 +234,53 @@ def quiz(item_id):
         random.shuffle(drops)
 
         return render_template(
-            "quiz/drag_drop.html", quiz=quiz_data, drags=drags, drops=drops, is_answered=is_answered
+            "quiz/drag_drop.html",
+            quiz=quiz_data,
+            drags=drags,
+            drops=drops,
+            is_answered=is_answered,
         )
 
     # mcq
     elif quiz_data["type"] == "mcq":
-        return render_template("quiz/mc.html", quiz=quiz_data, is_answered=is_answered, user_answer=user_answer)
+        return render_template(
+            "quiz/mc.html",
+            quiz=quiz_data,
+            is_answered=is_answered,
+            user_answer=user_answer,
+        )
 
 
 @app.route("/quiz/results/<int:item_id>")
 def results(item_id):
-    final_score = get_quiz_results()
-    return render_template("quiz/results.html", id=item_id, final_score=final_score)
+    if len(answers) != len(questions):
+        return "<h1>Please complete the quiz first!</h1>"
+
+    num_correct, num_wrong, percentage, results = get_quiz_results()
+    return render_template(
+        "quiz/results.html",
+        id=item_id,
+        final_score=[num_correct, num_wrong],
+        percentage=percentage,
+        results=results,
+    )
 
 
 # FUNCTIONS
 def get_quiz_results():
 
     if len(answers) != len(questions):
-        return
+        return "<h1>Please complete the quiz first!</h1>"
 
-    final_score = len(questions)
+    results = [True for _ in range(len(questions))]
+
+    num_correct = len(questions)
     for i in range(1, len(questions) + 1):
         # mcq
         if questions[i]["type"] == "mcq":
             if answers[i] != questions[i]["answer"]:
-                final_score -= 1
+                num_correct -= 1
+                results[i - 1] = False
 
         # drag and drop
         else:
@@ -225,9 +289,21 @@ def get_quiz_results():
             correct_ans = sorted(questions[i]["answer"].items())
 
             if user_ans != correct_ans:
-                final_score -= 1
+                num_correct -= 1
+                results[i - 1] = False
 
-    return f"{final_score} / {len(questions)}"
+    num_wrong = len(questions) - num_correct
+    percentage = f"{num_correct}0%"
+
+    return num_correct, num_wrong, percentage, results
+
+
+@app.route("/clear_quiz_answers", methods=["POST"])
+def clear_quiz_answers():
+    global answers
+    answers = {}
+
+    return "400"
 
 
 # AJAX FUNCTIONS
@@ -236,8 +312,8 @@ def save_answer():
     global answers
     answer = request.get_json()
 
-    quiz_id = int(answer['quizId'])
-    user_answer = answer['answer']
+    quiz_id = int(answer["quizId"])
+    user_answer = answer["answer"]
 
     answers[quiz_id] = user_answer
     return answer
